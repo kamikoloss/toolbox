7151f6de88d2c2873e4e7213cf504d8781d38edc
Kamiko Loss <info@gloxi.net>
2025-06-09 08:48:56 +0900

    子ページの構成を変えた

--- menu.js
+++ menu.js
@@ -1,14 +1,19 @@
 const menu = {
-    '/unixtime/': 'Unixtime',
+    '/unixtime.html': 'Unixtime',
 };
 const menuHtml = (link, text) => `<li><a href="${link}">${text}</a></li>`;
 const menuListHtml = Object.entries(menu).map(([link, text]) => menuHtml(link, text)).join('');
 
 document.getElementById('menu').innerHTML = /*html*/ `
-<h1>
-    <a href="/">GLOXINIA Toolbox</a>
-    <span id="subtitle">
-</h1>
-<ul id="menu">${menuListHtml}</ul>
+<h1><a href="/">GLOXINIA Toolbox</a></h1>
+<h2 id="page-name"></h2>
+<ul>${menuListHtml}</ul>
 <hr>
 `;
+
+let title = 'GLOXINIA Toolbox';
+if (pageName) {
+    title += pageName;
+    document.getElementById('page-name').innerHTML = pageName;
+}
+document.title = title;
