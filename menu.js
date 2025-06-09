c7fa43375cfe6778b2182d99ef22607bb328e09a
Kamiko Loss <info@gloxi.net>
2025-06-09 23:08:39 +0900

    Unixtime が動作するようにした

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+const siteName = 'GLOXINIA Tools';
 const menu = {
     '/unixtime.html': 'Unixtime',
 };
@@ -5,15 +6,16 @@ const menuHtml = (link, text) => `<li><a href="${link}">${text}</a></li>`;
 const menuListHtml = Object.entries(menu).map(([link, text]) => menuHtml(link, text)).join('');
 
 document.getElementById('menu').innerHTML = /*html*/ `
-<h1><a href="/">GLOXINIA Toolbox</a></h1>
+<h1><a href="/">${siteName}</a></h1>
 <h2 id="page-name"></h2>
 <ul>${menuListHtml}</ul>
 <hr>
 `;
 
-let title = 'GLOXINIA Toolbox';
-if (pageName) {
-    title += pageName;
+if (document.title == '') {
+    document.title = siteName;
+} else {
+    const pageName = document.title
+    document.title = `${siteName} - ${pageName}`;
     document.getElementById('page-name').innerHTML = pageName;
 }
-document.title = title;
